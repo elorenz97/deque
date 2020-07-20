fc373e313f17737f91a16c2787ab3ecd3bc570fd
elorenz97 <51880658+elorenz97@users.noreply.github.com>
2020-07-20 11:34:40 +0200

    Update deque-arr.c

--- array/deque-arr.c
+++ array/deque-arr.c
@@ -125,12 +125,14 @@ int first (deque * q)
 }
 
 void all (deque * q)
-{   int s = q->first;
+{  int s = q->first;
     for(int i = 0; i < q->count; i++)
     {
-    printf("------------");
-    printf("|%i|",q->values[s]);
-    printf("------------");
+    printf("------------\n");
+    printf("|%10i|",q->values[s]);
+    printf("\n");
+    printf("------------\n");
     s++;
     }
+
 }
